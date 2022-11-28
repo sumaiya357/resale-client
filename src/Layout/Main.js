a162358fcd81662beff88b8072cfb62724a55134
sumaiya246 <rahmansumaiya75@gmail.com>
2022-11-28 17:45:05 +0600

    footer setup done

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
 import NavBar from '../Shared/NavBar/NavBar';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <NavBar></NavBar>
              <Outlet></Outlet>
+             <Footer></Footer>
         </div>
     );
 };
