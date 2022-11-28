2a8a1380c374326a415b9752df63c78c73f0f2c8
sumaiya246 <rahmansumaiya75@gmail.com>
2022-11-28 17:29:43 +0600

    first commit

---
+++ src/Shared/NavBar/NavBar.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div>
+
+            {/* for small screen */}
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/signup'>SignUp</Link>
+                        </li>
+
+                        <li >
+                            <Link>
+                               My Orders
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link>
+                              Add a Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              My Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              All Seller
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                            All Buyer
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                             Reported Items
+                            </Link>
+                        </li>
+                        </ul>
+                    </div>
+
+                   
+                    <Link className="btn btn-ghost normal-case text-xl bg-error-content text-white">Furnishly</Link>
+                </div>
+                 
+
+                  {/* for large screen */}
+
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+
+                    <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/signup'>SignUp</Link>
+                        </li>
+
+                        <li >
+                            <Link>
+                               My Orders
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link>
+                              Add a Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              My Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              All Seller
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                            All Buyer
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                             Reported Items
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+            {/* ////////////////////// */}
+            {/* <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <Link className="btn btn-ghost normal-case text-xl bg-error-content text-white">Furnishly</Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0">
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                        <li>
+                            <Link to='/signup'>SignUp</Link>
+                        </li>
+
+                        <li >
+                            <Link>
+                               My Orders
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link>
+                              Add a Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              My Product
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                              All Seller
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                            All Buyer
+                            </Link>
+                        </li>
+                        <li>
+                            <Link>
+                             Reported Items
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div> */}
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+          primary: "#92400e",
+                  
+          secondary: "#0ea5e9",
+                  
+          accent: "#37CDBE",
+                  
+          neutral: "#3D4451",
+                  
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
