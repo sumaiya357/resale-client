669fa1e6b9204364c811bb0c811cdd508b1c74fe
sumaiya246 <rahmansumaiya75@gmail.com>
2022-11-30 19:26:19 +0600

    login form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState("");
+  
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='h-[500px] flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl'>Login</h2>
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                   
+                    <input {...register("firstName")} placeholder="First name" />
+                    <select {...register("category", { required: true })}>
+                        <option value="">Select...</option>
+                        <option value="A">Option A</option>
+                        <option value="B">Option B</option>
+                    </select>
+                    <textarea {...register("aboutYou")} placeholder="About you" />
+                    <p>{data}</p>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
