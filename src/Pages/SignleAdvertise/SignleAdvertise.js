c0ef57e9bf70b629caa436cf3c68eda935c97735
sumaiya357 <rahmansumaiya75@gmail>
2023-01-12 20:36:08 +0600

    advertised items added

--- src/Pages/SignleAdvertise/SignleAdvertise.js
+++ src/Pages/SignleAdvertise/SignleAdvertise.js
@@ -8,12 +8,19 @@ const SignleAdvertise = ({allitems}) => {
                 <figure><img src={allitems.img} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{allitems.name}</h2>
+
                     <span className="py-3"> <span className="text-1xl font-bold">Original Price:</span>{allitems.OriginalPrice}</span>
-                    <span className="py-3"><span className="text-1xl font-bold">Resale Price:</span> {allitems.ResalePrice}</span>
+
+                    <span className="py-3"><span className="text-1xl font-bold">Resale Price: </span> {allitems.ResalePrice}</span>
+
                     <span className="py-3"> <span className="text-1xl font-bold">Year of Use:</span> {allitems.YearofUse}</span>
+
                     <span className="py-3"><span className="text-1xl font-bold">Posting Time</span>{allitems.PostingTime}</span>
-                    <span className="py-3"><span className="text-1xl font-bold">Seller Name:</span> {allitems.SellerName}</span>
+                    <span className="py-3"><span className="text-1xl font-bold">Seller Name:</span> 
+                    {allitems.SellerName}</span>
+
                     <span className="py-3"><span className="text-1xl font-bold">location:</span>{allitems.location}</span>
+                    
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Book Now</button>
                     </div>
